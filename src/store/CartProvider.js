4545bfcdce4f5ffe0e5986313020555a7e0ade08
Ayaz-Rzayev <rzayev.ayaz198@gmail.com>
2022-02-12 23:23:46 +0400

    Logic to sum quantities of same food types  in cart added

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,31 +1,44 @@
-import React, {useReducer} from "react";
+import React, { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const defaultCart = {
+const defaultCartState = {
   items: [],
-  totalAmount: 0
-}
+  totalAmount: 0,
+};
 
-const cartReducer = (state, action) =>{
-  let newState
-  switch (action.type){
-    case 'ADD':
-      newState = {items: state.items.concat(action.item), totalAmount: state.totalAmount + action.item.price * action.item.amount}
-      break
-    default:
-      throw new Error ('sth wrong')
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex]
+    let updatedItems
+    if(existingCartItem){
+      const updatedItem = {
+        ...existingCartItem, amount: existingCartItem.amount + action.item.amount
+      }
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem
+    }else{
+      updatedItems = state.items.concat(action.item)
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
-  return newState
-}
+};
 
 const CartProvider = (props) => {
-  const [cartState, dispatchCart] = useReducer(cartReducer, defaultCart)
+  const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
 
   const addItemToCartHandler = (item) => {
-    dispatchCart({type: 'ADD', item: item})
+    dispatchCart({ type: "ADD", item: item });
   };
   const removeItemFromCartHandler = (item) => {
-    dispatchCart({type: 'REMOVE', id:item.id})
+    dispatchCart({ type: "REMOVE", id: item.id });
   };
 
   const cartContext = {
