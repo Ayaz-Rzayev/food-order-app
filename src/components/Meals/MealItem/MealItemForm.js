90082898f8ef09c5d5bf71b04647fb275ff67e41
Ayaz-Rzayev <rzayev.ayaz198@gmail.com>
2022-02-12 22:17:20 +0400

    add item to cart logic added

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,28 @@
-import React from "react"
+import React,{useRef} from "react"
 import styles from "./MealItemForm.module.css"
 import Input from "../../UI/Input"
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
+  const amountInputRef = useRef()
+  
+
+
+  const submitHandler =(event)=>{
+    event.preventDefault()
+    const enteredAmount = amountInputRef.current.value
+    const enteredAmountNum = +enteredAmount
+    if(enteredAmount.trim().length === 0 || enteredAmountNum < 1)
+    {
+      console.log("amount is invalid")
+      return
+    }
+    props.onAddToCart(enteredAmountNum)
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label = "Amount"
         input = {{
           id: 'amount',
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, {useReducer} from "react";
 import CartContext from "./cart-context";
 
+const defaultCart = {
+  items: [],
+  totalAmount: 0
+}
+
+const cartReducer = (state, action) =>{
+  let newState
+  switch (action.type){
+    case 'ADD':
+      newState = {items: state.items.concat(action.item), totalAmount: state.totalAmount + action.item.price * action.item.amount}
+      break
+    default:
+      throw new Error ('sth wrong')
+  }
+  return newState
+}
+
 const CartProvider = (props) => {
+  const [cartState, dispatchCart] = useReducer(cartReducer, defaultCart)
+
   const addItemToCartHandler = (item) => {
-    
+    dispatchCart({type: 'ADD', item: item})
   };
-  const removeItemFromCartHandler = (item) => {};
+  const removeItemFromCartHandler = (item) => {
+    dispatchCart({type: 'REMOVE', id:item.id})
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
