e15c85f350c0912909a2a8e483908ccacd2d47b6
Ayaz-Rzayev <rzayev.ayaz198@gmail.com>
2022-03-26 11:59:49 +0400

    Checkout component added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,26 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import CartContext from "../../store/cart-context";
 import styles from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+  const [displayOrderForm, setDisplayOrderForm] = useState(false);
+
   const cartCtx = useContext(CartContext);
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const cartItemRemoveHandler = (id) => {
-    cartCtx.removeItem(id)
-  }
+    cartCtx.removeItem(id);
+  };
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem({...item, amount:1})
+    cartCtx.addItem({ ...item, amount: 1 });
   };
 
   const cartItems = (
     <ul className={styles["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <CartItem key={item.id} name={item.name} amount = {item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)}/>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
+  const clickHandler = () => {
+    setDisplayOrderForm(true);
+  };
 
   return (
     <Modal onClose={props.onHideCart}>
@@ -29,11 +42,14 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
+      {displayOrderForm && <Checkout onCancel={props.onHideCart} />}
       <div className={styles.actions}>
         <button className={styles["button--alt"]} onClick={props.onHideCart}>
           Close
         </button>
-        <button className={styles.button}>Order</button>
+        <button className={styles.button} onClick={clickHandler}>
+          Order
+        </button>
       </div>
     </Modal>
   );
